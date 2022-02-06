d2b4f3cebf52201d4ef32685b9df24035b28fe46
Bojan Golic <bokigolic32@gmail.com>
2022-02-05 21:39:11 -0500

    aountinur work on src, add project, resume, nav...

---
+++ portfolio/src/components/Resume/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+function Resume() {
+  return (
+    <section class="container">
+      <h2 className="top-title">Resume</h2>
+      <hr></hr>
+
+      <div>
+        <div class="mt-5">
+          <h2 className="top-title">Bojan Golic</h2>
+          <ul>
+            <li>
+              Full-Stack Web Development
+            </li>
+          </ul>
+
+          <p class="mt-5">
+            <a href="https://www.linkedin.com/in/bojan-golic-35174120a/">
+              <img
+                src="#"
+                alt="linkedIn"
+              />
+            </a>
+          </p>
+
+          
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Resume;
